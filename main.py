1647588b2b65797eff073a3c6948b6a2774507f6
reyanp <reyanp352@gmail.com>
2026-05-30 19:52:52 -0500

    Included Token Usage in response.

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from dotenv import load_dotenv
 from google import genai
 
 
-def main():
+def main() -> None:
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    if api_key is None:
-        raise RuntimeError("GEMINI_API_KEY not set")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     client = genai.Client(api_key=api_key)
-
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
 
 
